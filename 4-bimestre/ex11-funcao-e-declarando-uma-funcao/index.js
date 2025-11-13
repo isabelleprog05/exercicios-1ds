4c5320b1b20c06151abc4ce8a89d5aa1465bba29
Isabelle <isabellelopes8n@gmail.com>
2025-11-13 22:29:14 +0000

    exercios

--- 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
+++ 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
@@ -1,4 +1,6 @@
 // CRIE UMA LÓGICA AQUI
-
+function boasVindas() {
+    console.log()
+}
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { boasVindas, soma }
--- 4-bimestre/ex12-usando-funcao/index.js
+++ 4-bimestre/ex12-usando-funcao/index.js
@@ -1,4 +1,13 @@
 // CRIE UMA LÓGICA AQUI
+function boasVindas() {
+ return ("Seja bem-vindo ao sistema!")
+} 
+
+function soma (a, b) {
+    return (a+b)
+}
+boasVindas()
+soma 
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { multiplicar, mostrarResultado }
