a3315871e6d09dc6968bcdb3967c517127f5be81
Isabelle <isabellelopes8n@gmail.com>
2025-11-13 22:34:00 +0000

    resolvido

--- 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
+++ 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
@@ -49,6 +49,5 @@ console.log(`
 
     Média geral das avaliações: ${mediaGeral}
 `)
-
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { filme1, filme2, filme3, filme4, filme5, filme6, filme7, filme8, filme9, filme10 }
